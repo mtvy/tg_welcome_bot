bf7621468cc3d81b88127585edef47321909dccc
mtvy <44533918+mtvy@users.noreply.github.com>
2022-10-04 08:27:59 +0300

    double handle delay

--- front/handle.py
+++ front/handle.py
@@ -76,7 +76,7 @@ def handle_mems() -> None:
 
     bot = TeleBot(TOKEN)
 
-    set_delay(10).seconds.do(_send_req, bot)
+    set_delay(20).seconds.do(_send_req, bot)
         
     while not exc:
         proc_run()
